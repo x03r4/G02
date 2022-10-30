f71b88326c9322babc115fdc16519eebbe5d3ee1
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-10-31 00:05:37 +0100

    the one with the changes on main

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ function NotFound() {
     <Layout>
     <div>
         <h1>404 - Page not found</h1>
-        <p>Sorry, this page doesn't exist</p>
+        <p>te zmiany są w mainie</p>
     </div>
     </Layout>
   )
