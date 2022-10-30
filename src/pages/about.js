f9bb43b43894d139b9e8284855da1a1bd25c6ab0
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-10-30 02:06:22 +0200

    lets change sth

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,12 +6,7 @@ function about() {
     <Layout>
       <div>
         <h1>About Page</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, vel
-          atque! Culpa natus, ea voluptatibus quis laborum reprehenderit rerum
-          est repudiandae, et dolore blanditiis sed nisi accusamus libero minima
-          explicabo!
-        </p>
+        <p>Lets change something</p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, vel
           atque! Culpa natus, ea voluptatibus quis laborum reprehenderit rerum
