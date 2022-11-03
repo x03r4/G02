621c9270624defc4a632f6a7510b0221e3ad1b5c
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-03 02:14:10 +0100

    added css modules

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import * as projects from "../../styles/projects.module.css"
 
 export default function Projects() {
   return (
     <Layout>
-      <div>Projects</div>
+      <div className={ projects.portfolio }>
+      <h2>Portfolio</h2>
+      <h3>Projects & Websites I've Created</h3>
+      </div>
     </Layout>
   )
 }
