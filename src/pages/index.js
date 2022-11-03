98319e9854e3fac437c987926aea82614c4a9ac3
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-03 02:36:24 +0100

    added file

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,16 @@ import * as styles from "../styles/home.module.css"
 export default function Home() {
   return (
     <Layout>
-      <section className={ styles.header }>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX Designer & Web Developer</p>
-          <Link to="/projects" className={ styles.btn }>My Portfolio Projects</Link>
+          <Link to="/projects" className={styles.btn}>
+            My Portfolio Projects
+          </Link>
         </div>
+        <img src="/banner.png" alt="banner" style={{ maxWidth:"100%"}}/>
       </section>
     </Layout>
   )
