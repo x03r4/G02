f35658cb1f23d07192e511b78c8160c9bf0de23f
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-04 01:34:00 +0100

    added graphql query inside Loyaout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import Navbar from './Navbar'
 import '../styles/global.css'
 
 export default function Layout({ children }) {
+  const data = useStaticQuery(graphql`
+    query siteInfo {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
+
+  const { author } = data.site.siteMetadata
+
   return (
     <div className='layout'>
         <Navbar />
@@ -10,7 +23,7 @@ export default function Layout({ children }) {
             { children }
         </div>
         <footer>
-            <p>Copyright 2022 - DJK</p>
+            <p>Copyright 2022 - { author }</p>
         </footer>
     </div>
   )
