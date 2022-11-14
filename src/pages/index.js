f9b8a36291a357ecff4f6c281b9d1767bec72b92
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-14 15:50:02 +0100

    l-17 added featured and thumbs images and fixed dependencies issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
+import Img from 'gatsby-image'
 
 export default function Home({ data }) {
   console.log(data)
   const { title, description, author } = data.site.siteMetadata
+  const { fluid } = data.file.childImageSharp
   return (
     <Layout>
       <section className={styles.header}>
@@ -13,26 +15,34 @@ export default function Home({ data }) {
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX Designer & Web Developer</p>
-           <p>{ title } - { description } - { author }</p>
+          <p>
+            {title} - {description} - {author}
+          </p>
           <Link to="/projects" className={styles.btn}>
             My Portfolio Projects
           </Link>
         </div>
-        <img src="/banner.png" alt="banner" style={{ maxWidth:"80%" }}/>
-       
+        <Img fluid={fluid} />
       </section>
     </Layout>
   )
-} 
+}
 
 export const query = graphql`
-query siteInfo {
-  site {
-    siteMetadata {
-      description
-      title
-      author
+  query siteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+        author
+      }
+    }
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
     }
   }
-}
 `
