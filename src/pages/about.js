ab3706855cee87e21f31f14e8d478050c2f603bd
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-16 12:12:01 +0100

    last commit before firs build

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,17 +6,9 @@ function about() {
     <Layout>
       <div>
         <h1>About Page</h1>
-        <p>Lets change something</p>
-        <p>
-          OK. Jestem w branchu o nazwie secondBranch
-          teraz dodam sobie commita 
-          na branchu secondBranch poszedł git rebase main pozniej checkout na main i git rebase secondBranch
-          teraz commit
-        </p>
-        <p>
-          rozwiązujemy zagadki gita <br />
-          one eternity later xD
-        </p>
+        <p>Now it's time to build and deploy this mini project</p>
+
+
       </div>
     </Layout>
   )
