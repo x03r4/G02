4ad04ecd6eae7853768beb1387b5bd9b5cbf98ca
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-16 10:56:24 +0100

    did stuff xD

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Img from 'gatsby-image'
 import * as styles from '../styles/project-details.module.css'
 
 function ProjectDetails() {
